a06e48cce9e2a8d0b062288e449855e04455fa6e
LuizTrMr <luizfelipetmoraes@hotmail.com>
2023-07-17 21:38:22 -0300

    Add some useful mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -33,6 +33,7 @@ vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true }) --vnoremap > >gv
 vim.api.nvim_set_keymap('n', '<Space>e', ':CocCommand explorer<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Space>f', ':CocCommand explorer --preset floating<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Space>j', '<C-w>w', { noremap = true }) -- Switch between opened explorer and file
+vim.api.nvim_set_keymap("n", "<Space><S-j>",  '<C-w><S-w>', { noremap = true })
 
 -- Lexplore
 vim.api.nvim_set_keymap('n', '<Leader>e', ':Lexplore<CR>', { noremap = true, silent = true })
@@ -53,5 +54,10 @@ vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : "<TAB>"', op
 vim.keymap.set("i", "<CR>", 'coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><c-r>=coc#on_enter()<CR>"', opts)
 vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
+-- Move lines up and down
 vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true })
+
+-- 
+vim.api.nvim_set_keymap('n', '<C-u>', "<C-u>zz", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-d>', "<C-d>zz", { noremap = true })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,6 +39,7 @@ return require('packer').startup(function(use)
 
 	use { 'mbbill/undotree' }
 
+	use { 'ziglang/zig.vim' } -- Zig syntax highlighter
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
