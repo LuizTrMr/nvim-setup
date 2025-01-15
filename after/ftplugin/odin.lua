9d78657dd759132a2ea880bffa71c8fcfdb6b7c0
LuizTrMr <luizfelipetmoraes@hotmail.com>
2025-01-15 09:52:41 -0300

    Add odin specific primitive types and highlighting

--- after/ftplugin/odin.lua
+++ after/ftplugin/odin.lua
@@ -1,3 +1,6 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 vim.cmd([[hi @variable.parameter.odin guifg='#c0caf5']])
 vim.cmd([[hi @variable.member.odin    guifg='#c0caf5']])
 vim.cmd([[hi @module.odin             guifg='#c0caf5']])
@@ -7,3 +10,4 @@ vim.cmd([[hi @keyword.odin            guifg='#7dcfff']])
 vim.cmd([[hi @keyword.return.odin     guifg='#7dcfff']])
 vim.cmd([[hi @keyword.type.odin       guifg='#7dcfff']])
 vim.cmd([[hi @type.odin               guifg='#2ac3de']])
+vim.cmd([[hi @odin_primitive_type     guifg='#2ac3de']])
